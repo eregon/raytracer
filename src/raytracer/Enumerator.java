47073331e094f362dfa01ebb1624c8adb17de41f
Benoit Daloze <eregontp@gmail.com>
2013-03-22 02:37:28 +0100

    radial enumerator

--- src/raytracer/Enumerator.java
+++ src/raytracer/Enumerator.java
@@ -237,3 +237,61 @@ class SkippingEnumerator extends Enumerator {
 		return current;
 	}
 }
+
+class RadialEnumerator extends Enumerator {
+	Enumerator iter;
+	int offset;
+	int n;
+	int h, w;
+	int mx, my;
+	boolean hasNext = true;
+	int current, next;
+	double a, b;
+
+	public RadialEnumerator(Enumerator iter, int offset, int n, int height, int width) {
+		this.iter = iter;
+		this.offset = offset;
+		this.n = n;
+		h = height;
+		w = width;
+		mx = w / 2 - 1;
+		my = h / 2 - 1;
+		// - Math.PI for [-PI; PI] range for atan2()
+		a = offset * 2 * Math.PI / n - Math.PI;
+		b = (offset + 1) * 2 * Math.PI / n - Math.PI;
+
+		generate();
+	}
+
+	@Override
+	public boolean hasNext() {
+		return hasNext;
+	}
+
+	private void generate() {
+		int x, y;
+		double angle;
+		do {
+			if (!iter.hasNext()) {
+				hasNext = false;
+				break;
+			}
+			next = iter.next();
+			x = next % w - mx;
+			y = next / w - my;
+			angle = Math.atan2(y, x);
+		} while (!(a < angle && angle <= b));
+	}
+
+	@Override
+	public Integer next() {
+		if (!hasNext)
+			throw new NoSuchElementException();
+
+		current = next;
+
+		generate();
+
+		return current;
+	}
+}
--- src/raytracer/RayTracer.java
+++ src/raytracer/RayTracer.java
@@ -33,8 +33,8 @@ public class RayTracer {
 				@Override
 				public void run() {
 					Ray ray = new Ray(scene.camera.position);
-					Enumerator iter = new SkippingEnumerator(
-							new CircleEnumerator(height, width), offset, nThreads);
+					Enumerator iter = new RadialEnumerator(
+							new CircleEnumerator(height, width), offset, nThreads, height, width);
 					for (int xy : iter) {
 						int x = xy % width, y = xy / width;
 						float a = x + 0.5f - width / 2f;
