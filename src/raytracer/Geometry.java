f74be86db824ed99f7ba3e8ad1daf79fc4d1acff
Benoit Daloze <eregontp@gmail.com>
2013-04-10 19:31:21 +0200

    be precise on roles

--- src/raytracer/Geometry.java
+++ src/raytracer/Geometry.java
@@ -1,5 +1,9 @@
 package raytracer;
 
 public interface Geometry extends Surface {
+	/** Closest intersection, must set distance, point and normal if found */
+	@Override
+	public Intersection intersection(Ray ray);
+
 	public Box computeBoundingBox(Transformation transformation);
 }
